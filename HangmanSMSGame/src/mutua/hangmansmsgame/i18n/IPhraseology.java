a22ded2a83dd01253224f55552dc4a6fea5c48b3
luiz <luiz@e8318a20-2124-4f52-a82c-d3615043beb2>
2014-12-31 18:18:02 +0000

    Advances on the game implementation -- phraseologies
    git-svn-id: file:///home/luiz/backup/RArchPi/raid0/svn/Projetos/Mutua/SourceCodes/celltick/SMSGames@185 e8318a20-2124-4f52-a82c-d3615043beb2

--- HangmanSMSGame/src/mutua/hangmansmsgame/i18n/IPhraseology.java
+++ HangmanSMSGame/src/mutua/hangmansmsgame/i18n/IPhraseology.java
@@ -25,6 +25,18 @@ public abstract class IPhraseology {
 		return new TestPhraseology();
 	}
 
+	protected String getList(String[] elements, String pairSeparator, String lastPairSeparator) {
+        String list = "";
+        for (int i=0; i<elements.length; i++) {
+                list += elements[i];
+                if (i == (elements.length-2)) {
+                        list += lastPairSeparator;
+                } else if (i != (elements.length-1)) {
+                        list += pairSeparator;
+                }
+        }
+        return list;
+}
 	
 	/**********************
 	** INTERFACE METHODS **
@@ -33,27 +45,27 @@ public abstract class IPhraseology {
 	// CUPONS
 	/////////
 	
-	/** shown when the user deserves another lucky number in order to participate in the next draw(s) */
-	public abstract String CUPOMNewLuckyNumber(String cuponsList, String series, String daysList, String monthAndYear);
-	
-	/** shown when the user wants to get informed on which draws him/her participation is assured */
-	public abstract String CUPOMDrawParticipation(String daysList, String monthAndYear, String cuponsList);
-	
-	/** shown when the user wants to be informed about draws on which he/she is a participant, but he/she isn't participating on any */
-	public abstract String CUPOMNoLuckyNumbers();
+//	/** shown when the user deserves another lucky number in order to participate in the next draw(s) */
+//	public abstract String CUPOMNewLuckyNumber(String cuponsList, String series, String daysList, String monthAndYear);
+//	
+//	/** shown when the user wants to get informed on which draws him/her participation is assured */
+//	public abstract String CUPOMDrawParticipation(String daysList, String monthAndYear, String cuponsList);
+//	
+//	/** shown when the user wants to be informed about draws on which he/she is a participant, but he/she isn't participating on any */
+//	public abstract String CUPOMNoLuckyNumbers();
 	
 	
 	// SUBSCRIPTION
 	///////////////
 	
-	/** shown when the user requested to be a subscriber, but he/she is already one */
-	public abstract String SUBSCRIPTIONAlreadySubscribed();
-	
-	/** shown when we could not bill the user and, therefore, could not complete the subscription or renewal process */
-	public abstract String SUBSCRIPTIONCouldNotBillNorCompleteTheSubscriptionOrRenewal();
-	
-	/** show when the time to renew the subscription has come and it succeeded */
-	public abstract String SUBSCRIPTIONRenewalSuccess();
+//	/** shown when the user requested to be a subscriber, but he/she is already one */
+//	public abstract String SUBSCRIPTIONAlreadySubscribed();
+//	
+//	/** shown when we could not bill the user and, therefore, could not complete the subscription or renewal process */
+//	public abstract String SUBSCRIPTIONCouldNotBillNorCompleteTheSubscriptionOrRenewal();
+//	
+//	/** show when the time to renew the subscription has come and it succeeded */
+//	public abstract String SUBSCRIPTIONRenewalSuccess();
 	
 	
 	// INFO
@@ -67,5 +79,108 @@ public abstract class IPhraseology {
 
 	/** shown when the user request the help / instructions */
 	public abstract String[] INFOFullHelp();
+	
+	
+	// PROFILE
+	//////////
+	
+	/** shown when a user wants to view the profile of another user */
+	public abstract String PROFILEView(String nick, String state, int numberOfLuckyNumbers);
 
+	/** shown when the game asks the player to choose a nickname */
+	public abstract String PROFILEFullfillingAskNick();
+
+
+	
+	// PLAYING
+	//////////
+	
+	protected String getHangDrawing(boolean drawHead, boolean drawLeftArm, boolean drawRightArm,
+	                                boolean drawChest, boolean drawLeftLeg, boolean drawRightLeg) {
+		return "+-+\n" +
+		       "| " + (drawHead?"O":"") + "\n" +
+		       "|" + (drawLeftArm?"/":" ") + (drawChest?"|":" ") + (drawRightArm?"\\":"") + "\n" +
+		       "|" + (drawLeftLeg?"/":" ") + " " + (drawRightLeg?"\\":"") + "\n" +
+		       "|\n" +
+		       "====\n";
+	}
+	
+	protected String getWinningDrawing() {
+		return "\\0/\n" +
+		       " |\n" +
+		       "/ \\\n";
+	}
+	
+	protected String getLoosingDrawing() {
+		return "+-+\n" +
+		       "| x\n" +
+		       "|/|\\\n" +
+		       "|/ \\\n" +
+		       "====\n";
+	}
+	
+	/** shows the state of the game play to the player attempting to guess the word */
+	public abstract String PLAYINGWordGuessingPlayerStatus(boolean drawHead, boolean drawLeftArm, boolean drawRightArm,
+                                                           boolean drawChest, boolean drawLeftLeg, boolean drawRightLeg,
+                                                           String guessedWordSoFar, String usedLetters);
+	
+	/** shows the state of the game play to the player who provided the word */
+	public abstract String PLAYINGWordProvidingPlayerStatus(boolean drawHead, boolean drawLeftArm, boolean drawRightArm,
+	                                                        boolean drawChest, boolean drawLeftLeg, boolean drawRightLeg,
+	                                                        String guessedWordSoFar, String guessedLetter, String usedLetters, String nick);
+	
+	/** shown to the winning player, who tried to guess the word */
+	public abstract String PLAYINGWinningMessageForWordGuessingPlayer(String word, String luckyNumber);
+	
+	/** show to the word providing player when the word guessing player won the game */
+	public abstract String PLAYINGWinningMessageForWordProvidingPlayer(String nick);
+	
+	/** shown to the losing player, who tried to guess the word 
+	 * @param nick TODO*/
+	public abstract String PLAYINGLoosingMessageForWordGuessingPlayer(String word, String nick);
+	
+	/** show to the word providing player when the word guessing player lost the game */
+	public abstract String PLAYINGLoosingMessageForWordProvidingPlayer(String nick);
+	
+	
+	// INVITING
+	///////////
+	
+	// TODO what if the user provide an unknown nickname or a wrong phone number?
+	/** shown when the game wants to ask the player for an opponent to be invited for a match */
+	public abstract String INVITINGAskOpponentNickOrPhone(String invitingPlayerNickname);
+
+	/** shown when the game asks the user for a word to start a match with an invited player, for which the nick name was provided */
+	public abstract String INVITINGAskForAWordToStartAMatchBasedOnOpponentNickInvitation(String opponentNickname);
+
+	/** shown when the game asks the user for a word to start a match with an invited player, for which the phone number was provided */
+	public abstract String INVITINGAskForAWordToStartAMatchBasedOnOpponentPhoneInvitation(String opponentPhoneNumber);
+
+	/** message to notify the inviting player that the invitation has been sent */
+	public abstract String INVITINGInvitationNotificationForInvitingPlayer(String invitedPlayerNickName);
+	
+	/** message to notify the inviting player that the opponent took too long to reply and the match is cancelled */
+	public abstract String INVITINGTimeoutNotificationForInvitingPlayer(String invitedPlayerNickName, String suggestedNewPlayersNickName);
+	
+	/** message to notify the invited player that someone wants to play a hangman match */
+	public abstract String INVITINGInvitationNotificationForInvitedPlayer(String invitingPlayerNickName);
+	
+
+	// LISTING PLAYERS
+	//////////////////
+	
+	/** shows a list of players. 'playersInfo' := { {nick, estate, number of lucky numbers received}, ... } */
+	public abstract String LISTINGShowPlayers(String[][] playersInfo);
+	
+	
+	// PROVOKING
+	////////////
+	
+	// TODO what if 'destinationNick' is invalid?
+	/** presented to the user after he/she attempted to provoke someone, indicating the message was delivered */
+	public abstract String PROVOKINGDeliveryNotification(String destinationNick);
+
+	/** the message sent to the destination user when he is being provoked by someone */
+	public abstract String PROVOKINGSendMessage(String nick, String message);
+	
 }
--- HangmanSMSGame/src/mutua/hangmansmsgame/i18n/TestPhraseology.java
+++ HangmanSMSGame/src/mutua/hangmansmsgame/i18n/TestPhraseology.java
@@ -14,7 +14,12 @@ package mutua.hangmansmsgame.i18n;
 
 public class TestPhraseology extends IPhraseology {
 
-	@Override
+	
+	private String getShortHelp() {
+		return "(J) Play online; (C) Invite a friend or user; (R)anking; (A)Help";
+	}
+	
+/*	@Override
 	public String CUPOMNewLuckyNumber(String cuponsList, String series, String daysList, String monthAndYear) {
 		return null;
 	}
@@ -31,19 +36,19 @@ public class TestPhraseology extends IPhraseology {
 
 	@Override
 	public String SUBSCRIPTIONAlreadySubscribed() {
-		return null;
+		return "HANGMAN: you're already a subscriber. Next renewal in DD/MM/YYYY. Send LIST to XXXX to see online players; P NICK MSG to provoke them! To cancel the service, send EXIT";
 	}
 
 	@Override
 	public String SUBSCRIPTIONCouldNotBillNorCompleteTheSubscriptionOrRenewal() {
-		return null;
+		return "HANGMAN: it was not possible to confirm or renew your subscription. Please, check your credits and try again. Send SIGNUP to XXXX ($2+taxes/week)";
 	}
 
 	@Override
 	public String SUBSCRIPTIONRenewalSuccess() {
-		return null;
+		return "HANGMAN: your subscription has been renewed and you can send provocation messages for free for 1 more week. Send LIST to XXXX to see online players; P NICK MSG to provoke";
 	}
-
+*/
 	@Override
 	public String INFOWelcome() {
 		return "Welcome to the HANGMAN game. Join and compete for prizes. Send HELP for free to XXXX to know the rules.";
@@ -60,7 +65,110 @@ public class TestPhraseology extends IPhraseology {
 
 	@Override
 	public String INFOWelcomeMenu() {
-		return "Pick an option. Send to XXXX: (J) Play online; (C) Invite a friend or user; (R)anking; (A)Help";
+		return "Pick an option. Send to XXXX: " + getShortHelp();
+	}
+
+	@Override
+	public String PROFILEView(String nick, String state, int numberOfLuckyNumbers) {
+		return "HANGMAN: DeiaGATA: Subscribed, Rio de Janeiro, 109 lucky numbers. Send SIGNUP to provoke for free or INVITE DeiaGATA for a match.";
+	}
+	
+	@Override
+	public String PROFILEFullfillingAskNick() {
+		return "HANGMAN: To play with a friend, u need first to sign your name. Now send your name (8 letters or numbers max.) to XXXXX";
+	}
+
+	@Override
+	public String PLAYINGWordGuessingPlayerStatus(boolean drawHead, boolean drawLeftArm, boolean drawRightArm,
+	                                              boolean drawChest, boolean drawLeftLeg, boolean drawRightLeg,
+	                                              String guessedWordSoFar, String usedLetters) {
+		return getHangDrawing(drawHead, drawLeftArm, drawRightArm, drawChest, drawLeftLeg, drawRightLeg) +
+		       "Word: " + guessedWordSoFar + "\n" +
+		       "Used: " + usedLetters + "\n" +
+		       "Send a letter, the complete word or END to cancel the game";
+	}
+
+	@Override
+	public String PLAYINGWordProvidingPlayerStatus(boolean drawHead, boolean drawLeftArm, boolean drawRightArm,
+	                                               boolean drawChest, boolean drawLeftLeg, boolean drawRightLeg,
+	                                               String guessedWordSoFar, String guessedLetter, String usedLetters,
+	                                               String nick) {
+		return nick + " guessed letter " + guessedLetter + "\n" +
+		       getHangDrawing(drawHead, drawLeftArm, drawRightArm, drawChest, drawLeftLeg, drawRightLeg) +
+		       "Word: " + guessedWordSoFar + "\n" +
+		       "Used: " + usedLetters + "\n" +
+		       "Send P " + nick + " MSG to provoke (him/her)";
+	}
+
+	@Override
+	public String PLAYINGWinningMessageForWordGuessingPlayer(String word, String luckyNumber) {
+		return getWinningDrawing() + word +
+		       "! You got it! Here is your lucky number: " + luckyNumber + ". Send: J to play or A for help";
+	}
+
+	@Override
+	public String PLAYINGWinningMessageForWordProvidingPlayer(String nick) {
+		return nick + " guessed your word! P " + nick + " MSG to provoke him/her or INVITE " + nick + " for a new match";
+	}
+
+	@Override
+	public String PLAYINGLoosingMessageForWordGuessingPlayer(String word, String nick) {
+		return getLoosingDrawing() + "The word was " + word + ". " + getShortHelp();
+		// TODO: the message may encourage the loosing player to try and suggest a word back to the opponent.
+	}
+
+	@Override
+	public String PLAYINGLoosingMessageForWordProvidingPlayer(String nick) {
+		return "Good one! " + nick + " wasn't able to guessed your word! P " + nick + " MSG to provoke him/her or INVITE " + nick + " for a new match";
+	}
+
+	@Override
+	public String INVITINGAskOpponentNickOrPhone(String invitingPlayerNickname) {
+		return "HANGMAN: Name registered: " + invitingPlayerNickname + ". Send your friend's phone to XXXX or LIST to see online players. NICK <NEW NICK> to change your name.";
+	}
+
+	@Override
+	public String INVITINGAskForAWordToStartAMatchBasedOnOpponentNickInvitation(String opponentNickname) {
+		return "HANGMAN: Inviting " + opponentNickname + ". Think of a word without special digits and send it now to XXXX. After the invitation, you'll get a lucky number";
+	}
+
+	@Override
+	public String INVITINGAskForAWordToStartAMatchBasedOnOpponentPhoneInvitation(String opponentPhoneNumber) {
+		return "HANGMAN: Your friend's phone: " + opponentPhoneNumber + ". Think of a word without special digits and send it now to XXXX. After the invitation, you'll get a lucky number";
+	}
+
+	@Override
+	public String INVITINGInvitationNotificationForInvitingPlayer(String invitedPlayerNickName) {
+		return invitedPlayerNickName + " was invited to play with you. while you wait, you can provoke " + invitedPlayerNickName +  " by sending a message to XXXX (0.31+tax) or send SIGNUP to provoke for free how many times you want";
+	}
+
+	@Override
+	public String INVITINGTimeoutNotificationForInvitingPlayer(String invitedPlayerNickName, String suggestedNewPlayersNickName) {
+		return invitedPlayerNickName + " is taking too long to answer. However, a new player, " +
+		       suggestedNewPlayersNickName + ", is available. Play with " + suggestedNewPlayersNickName + "? Send YES to XXXX";
+	}
+
+	@Override
+	public String INVITINGInvitationNotificationForInvitedPlayer(String invitingPlayerNickName) {
+		return "HANGMAN: " + invitingPlayerNickName + " is inviting you for a hangman match. Do you accept? Send YES to XXXXX or PROFILE to see " + invitingPlayerNickName + " information";
+	}
+
+	@Override
+	public String LISTINGShowPlayers(String[][] playersInfo) {
+		String playerTemplate = "\\1 (\\2/\\3)";
+		String[] players = {"BrunoCBO (RJ/64)", "DeiaGATA (SP/89)", "Doido (BA/102)", "zaza (RS/49)"};
+		String playersList = getList(players, ", ", ", ");
+		return playersList + ". To play, send INVITE NICK to XXXXX; MORE for more players or PROFILE NICK";
+	}
+
+	@Override
+	public String PROVOKINGDeliveryNotification(String destinationNick) {
+		return "Your message was sent to " + destinationNick + ". Wait for the answer or provoke other players sending P NICK MSG to XXXXX. Send SIGNUP to provoke for free.";
+	}
+
+	@Override
+	public String PROVOKINGSendMessage(String nick, String message) {
+		return nick + ": " + message + " - Answer by sending P " + nick + " MSG to XXXX";
 	}
 	
 }
