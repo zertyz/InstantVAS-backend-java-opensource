215d57661b0e23d7cc2b8c94520b076eb5f008f7
luiz <luiz@e8318a20-2124-4f52-a82c-d3615043beb2>
2015-01-08 19:41:01 +0000

    Celltick -- almost ready first version
    git-svn-id: file:///home/luiz/backup/RArchPi/raid0/svn/Projetos/Mutua/SourceCodes/celltick/SMSGames@194 e8318a20-2124-4f52-a82c-d3615043beb2

---
+++ SMSInCelltick/src/mutua/smsin/parsers/SMSInCelltick.java
@@ -0,0 +1,90 @@
+package mutua.smsin.parsers;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mutua.smsin.dto.IncomingSMSDto;
+import mutua.smsin.dto.IncomingSMSDto.ESMSInParserCarrier;
+import mutua.smsout.senders.SMSOutCelltick;
+
+/** <pre>
+ * SMSInCelltick.java
+ * ==================
+ * (created by luiz, Jan 8, 2015)
+ *
+ * This class is responsible for implementing the passive MO notification api for Celltick
+ * gateways
+ *
+ * @see RelatedClass(es)
+ * @version $Id$
+ * @author luiz
+ */
+
+public class SMSInCelltick extends SMSInParser<HttpServletRequest, HttpServletResponse> {
+
+	public SMSInCelltick(String smsAppId) {
+		super("SMSInCelltick", smsAppId);
+	}
+
+	@Override
+	public IncomingSMSDto parseIncomingSMS(HttpServletRequest request) {
+		// http://localhost:8080/HangmanSMSGameServices/AddToMOQueue?AUTHENTICATION_TOKEN=...&MSISDN=(+55)?DDN?NNNNNNNN&CARRIER_NAME=...&LA=...&MO_ID=...&TEXT=...
+		String msisdn       = request.getParameter("MSISDN");
+		String carrierName  = request.getParameter("CARRIER_NAME");
+		String largeAccount = request.getParameter("LA");
+		String moId         = request.getParameter("MO_ID");
+		String text         = request.getParameter("TEXT");
+		
+		if ((msisdn == null) || (carrierName == null) || (largeAccount == null) || (moId == null) || (text == null)) {
+			return null;
+		}
+		
+		// extra parameters
+		String account  = request.getParameter("account");
+		String validity = request.getParameter("validity");
+		String smsc     = request.getParameter("smsc");
+		
+		// TODO fix this workarround (if it is really necessary and if not, remove the dependency from SMSOutCelltick project)
+		SMSOutCelltick.ACCOUNT  = account;
+		SMSOutCelltick.VALIDITY = validity;
+		SMSOutCelltick.SMSC     = smsc;
+		// TODO this should definitely not be here
+		SMSOutCelltick.shortCode= largeAccount;
+		
+		
+		ESMSInParserCarrier carrier = ESMSInParserCarrier.valueOf(carrierName.toUpperCase());
+		if (carrier == null) {
+			carrier = ESMSInParserCarrier.UNKNOWN;	// probably will make 'sendReply' return 'REJECTED'
+		}
+		
+		return new IncomingSMSDto(msisdn, text, carrier, largeAccount, moId);
+	}
+
+	@Override
+	public void sendReply(ESMSInParserSMSAcceptionStatus status, HttpServletResponse response) {
+		try {
+			response.setContentType("text/plain");
+			PrintStream out = new PrintStream(response.getOutputStream());
+			switch (status) {
+				case ACCEPTED:
+					out.print("ACCEPTED");
+					break;
+				case POSTPONED:
+					out.print("POSTPONED");
+					break;
+				default:
+					//Instrumentation.reportLocalopInconsistency("Unimplemented switch case statement '"+status.toString()+"'");
+			        // now go on performing as rejected
+				case REJECTED:
+					out.print("REJECTED");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("IOException while acknowledging to an MO", e);
+		}
+	}
+
+}
---
+++ SMSOutCelltick/src/mutua/smsout/senders/SMSOutCelltick.java
@@ -0,0 +1,67 @@
+package mutua.smsout.senders;
+
+import java.io.IOException;
+
+import adapters.HTTPClientAdapter;
+import adapters.dto.HTTPRequestDto;
+import mutua.smsout.dto.OutgoingSMSDto;
+
+/** <pre>
+ * SMSOutCelltick.java
+ * ===================
+ * (created by luiz, Jan 8, 2015)
+ *
+ * Send MT SMSes through Celltick gateways
+ *
+ * @see RelatedClass(es)
+ * @version $Id$
+ * @author luiz
+ */
+
+public class SMSOutCelltick extends SMSOutSender {
+
+	
+	// CONFIGURATION
+	////////////////
+	
+	public static String MT_URL = "http://localhost:15001/cgi-bin/sendsms";
+	public static int    NUMBER_OF_RETRY_ATTEMPTS = 5;
+	public static long   DELAY_BETWEEN_ATTEMPTS   = 5000;
+	
+	public static String ACCOUNT;
+	public static String VALIDITY;
+	public static String SMSC;
+
+	// TODO fix this fix for the "we don't know the short code problem", removing the public and static and relying solo on the constructor passed info
+	public static String shortCode;
+	
+	public SMSOutCelltick(String smsAppId, String shortCode) {
+		super("SMSOutCelltick", smsAppId, NUMBER_OF_RETRY_ATTEMPTS, DELAY_BETWEEN_ATTEMPTS);
+		SMSOutCelltick.shortCode = shortCode;
+	}
+
+	@Override
+	public EOutgoingSMSAcceptionStatus rawSendMessage(OutgoingSMSDto smsOut) throws IOException {
+		HTTPRequestDto requestData = new HTTPRequestDto();
+		requestData.addParameter("to",        smsOut.getPhone());
+		requestData.addParameter("total",     "1");
+		requestData.addParameter("text",      smsOut.getText());
+		requestData.addParameter("subAct",    "1");
+		requestData.addParameter("from",      shortCode);
+		requestData.addParameter("smsc",      SMSC);
+		requestData.addParameter("password",  "celltick");
+		requestData.addParameter("username",  "celltick");
+		requestData.addParameter("validity",  VALIDITY);
+		requestData.addParameter("bundle",    "R1A");
+		requestData.addParameter("account",   ACCOUNT);
+		requestData.addParameter("dlrmask",   "2");
+		requestData.addParameter("reportDLR", "0");
+		String response = HTTPClientAdapter.requestGet(MT_URL, requestData, "UTF-8");
+		if (response.indexOf("Sent") != -1) {
+			return EOutgoingSMSAcceptionStatus.ACCEPTED;
+		} else {
+			return EOutgoingSMSAcceptionStatus.POSTPONED;
+		}
+	}
+
+}
