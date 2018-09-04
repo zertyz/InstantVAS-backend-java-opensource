e5499430e93e87fcae42bdfb7b269a099db9ebc7
Luiz Silveira <zertyz@gmail.com>
2018-09-04 20:27:51 -0300

    Updated Mutua's DAL Factory Pattern

--- MutuaSMSAppModuleDALProfile/src/mutua/smsappmodule/dal/SMSAppModuleDALFactoryProfile.java
+++ MutuaSMSAppModuleDALProfile/src/mutua/smsappmodule/dal/SMSAppModuleDALFactoryProfile.java
@@ -11,16 +11,20 @@ import mutua.smsappmodule.dal.mvstore.ProfileDB;
  * ==================================
  * (created by luiz, Aug 3, 2015)
  *
- * Enum based implementation of the Factory Pattern, to select among
- * data access layers
+ * Enum based implementation of the Mutua's DAL Factory Pattern, to select among
+ * data access layers.
  *
- * @see RelatedClass(es)
+ * @see SMSAppModuleDALFactory
  * @version $Id$
  * @author luiz
  */
 
 public enum SMSAppModuleDALFactoryProfile {
 	
+
+	// DALs
+	///////
+
 	RAM {
 		@Override
 		protected void instantiateDataAccessLayers() {
@@ -44,29 +48,60 @@ public enum SMSAppModuleDALFactoryProfile {
 	
 	;
 	
-//	@ConfigurableElement("The desired data access handler for the 'User Profile SMS Module' facilities")
-//	public static SMSAppModuleDALFactoryProfile DEFAULT_DAL = SMSAppModuleDALFactoryProfile.RAM;
+
+	// Data Access Objects & methods
+	////////////////////////////////
 	
 	private IProfileDB profileDB;
-	
-	private boolean wasInstantiated = false;
-	
-	/** method to construct the DAO instances */
-	protected abstract void instantiateDataAccessLayers() throws SQLException;
-	
-	/** this method allows the instantiation of only the desired data access layer
-	/* (preventing unecessary drivers to be loaded) */
-	public void checkDataAccessLayers() {
-		if (!wasInstantiated) try {
-			instantiateDataAccessLayers();
-			wasInstantiated = true;
-		} catch (Throwable t) {
-			Instrumentation.reportThrowable(t, "Error instantiating 'SMSAppModuleDALProfile'");
-		}
-	}
 	
 	public IProfileDB getProfileDB() {
 		checkDataAccessLayers();
 		return profileDB;
 	}
+
+	/** Method to construct the DAO instances, which is Overridden by each enum. */
+	protected abstract void instantiateDataAccessLayers() throws SQLException;
+	
+
+	// Instantiation Parameters
+	///////////////////////////
+	//
+	// Some DALs require specific parameters to be instantiated -- for instance, what is the name of the Queue entry I should refer to?
+	// The following fields controls this optional behavior
+
+	// ... the protected parameters list that should be overwritten
+	
+	/** Method to setup the soon-to-be-instantiated Data Access Objects provided by this DAL.
+	 * 
+	 *  Not all DALs need it. This one doesn't, for instance.
+	 *  
+	 *  See {@link SMSAppModuleDALFactoryChat} for a good example.
+	 *  
+	 *  @param ...     see {@link #... parameter name at 'Instantiation Parameters' session} */
+//	public SMSAppModuleDALFactory setInstantiationParameters(...) {
+//		this...     = ...;
+//		this.wasInstantiationParametersProvided = true;
+//		return this;
+//	}
+	
+	private static final boolean requireInstantiationParameters = false;
+	private boolean wasInstantiationParametersProvided = false;
+	private boolean wasInstantiated                    = false;
+
+	/** This method allows the instantiation of only the desired data access layer
+	/* and their set of DAOs, preventing unnecessary drivers to be loaded.
+	 * ... and checks if {@link #setInstantiationParameters} was properly called. */
+	public void checkDataAccessLayers() {
+		if (!wasInstantiated) try {
+			// checks if {@link #setInstantiationParameters} was properly called
+			if (requireInstantiationParameters && (wasInstantiationParametersProvided == false)) {
+				throw new RuntimeException("Mutua's DAL Factory pattern: '"+this.getClass().getName()+"."+this.name()+".setInstantiationParameters(...)' was not properly called at DATA_ACCESS_LAYERs setup.");
+			}
+			instantiateDataAccessLayers();
+			wasInstantiated = true;
+		} catch (Throwable t) {
+			Instrumentation.reportThrowable(t, "Error instantiating '"+this.getClass().getName()+"'");
+		}
+	}
+	
 }
